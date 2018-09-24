d804737c4486787debed9bbeabc5a32f06f8d9e7
christophe le drean <christophe@rouge-rose.net>
2018-09-24 12:41:42 +0200

    Ajouter les filtres relatifs aux numéros

--- vnumeros_fonctions.php
+++ vnumeros_fonctions.php
@@ -4,32 +4,36 @@ if (!defined("_ECRIRE_INC_VERSION")) {
 	return;
 }
 
-
-function vnumeros_liste_intervalles($numeros_debut_fin) {
-	$numeros_nombre = str_replace('v', '', $numeros_debut_fin);
-	
-	// tous les numéros d'un abonnement
-	$references = array_map(
-		function($n) {return sprintf('v%04d', $n);},
-		range(reset($numeros_nombre), end($numeros_nombre), 1)
-	);
+/**
+ * Intervalles des références de numéro entre un numéro début et un numéro fin.
+ * 
+ * @param  array $numeros_debut_fin numero_debut, numero_fin
+ * @return array la clé de chaque élément est équivalente à chaque valeur.
+ */
+function filtre_numeros_liste_intervalles($numeros_debut_fin) {
+	// tous les numéros entre deux références
+	$references = range(reset($numeros_nombre), end($numeros_nombre), 1);
 	
 	$liste = array();
 	
 	foreach ($references as $reference) {
-		$liste[$reference] = vnumeros_convertir_reference_titre($reference);
+		$liste[$reference] = $reference;
 	}
 	
 	return $liste;
 }
 
 
-function vnumeros_convertir_reference_titre($reference) {
-	$nombre = intval(str_replace('v', '', $reference));
-	
-	if ($nombre) {
-		return $titre = 'Vacarme '.$nombre;
-	} else {
-		return '';
-	}
+/**
+ * Traduire la durée d'abonnement en nombre de numéros
+ * La fonction retourne également la chaîne "numéro(s)".
+ * 
+ * @param  string $periodicite (12|24 month)
+ * @return string
+ */
+function filtre_numeros_nombre_en_clair($duree) {
+	$periodicite = _VACARME_PERIODICITE;
+	$nb = intval($duree) / $periodicite;
+	$numeros = ($nb == 1 ? _T('vnumeros:numero') : _T('vnumeros:numeros', array('nb' => $nb)));
+	return $numeros;
 }
--- vnumeros_options.php
+++ vnumeros_options.php
@@ -5,6 +5,10 @@ if (!defined("_ECRIRE_INC_VERSION")) {
 }
 
 // nombre de numéros publiés dans l'année
-if (!defined('_VACARME_PERIODICITE_NUMEROS')){
-	define('_VACARME_PERIODICITE_NUMEROS', 4);
+if (!defined('_VACARME_NOMBRE_NUMEROS')) {
+	define('_VACARME_NOMBRE_NUMEROS', 4);
+}
+
+if (!defined('_VACARME_PERIODICITE')) {
+	define('_VACARME_PERIODICITE', 3);
 }
