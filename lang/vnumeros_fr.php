eaeb043961d24677df6ba9b32987c290a8bc7c84
christophe le drean <christophe@rouge-rose.net>
2019-02-16 16:29:55 +0100

    Corriger texte référence numéro :
    Le texte relatif à la référence d'un numéro était faux.

--- lang/vnumeros_fr.php
+++ lang/vnumeros_fr.php
@@ -17,32 +17,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_taxe_explication' => "TVA par défaut applicable aux numéros. Saisie de la forme 0.055 pour une TVA à 5.5% par exemple.",
 	'cfg_taxe_label' => "TVA",
 	'cfg_titre_parametrages' => 'Paramétrages',
-	
-	// D 
+
+	// D
 	'date_numero_label' => "Date de sortie du numéro",
 	'date_numero_explication' => "Cette date permettra de déduire la saison et l'année du numéro. Pour mémoire : printemps, à partir du 21 mars ; été, 21 juin ; automne, 21 sept. ; hiver, 21 déc.",
-	
+
 	// I
 	'isbn_explication' => "13 chiffres, sans tiret",
 	'isbn_label' => "Numéro ISBN",
-	
+
 	'numero' => "1 numéro",
 	'numeros' => "@nb@ numéros",
-	
+
 	// P
 	'pages_total_label' => "Nombre de pages",
 	'prix_ht_label' => "Prix HT",
-	
+
 	// R
 	'redacteurchef_label' => "Rédaction en chef",
-	'reference_explication' => 'Exemple pour Vacarme 83 : v0083. Soit lettre v suivie du numéro sur 4 chiffres',
+	'reference_explication' => 'Exemple pour Vacarme 83 : 83. Soit le nombre qui identifie le numéro.',
 	'reference_label' => 'Référence du numéro',
 
 	// T
 	'taxe_explication' => "TVA applicable. La valeur pré-saisie est celle de la configuration",
 	'titre_page_configurer_vnumeros' => 'Configuration des numéros',
 	'titre_config_reference_update' => 'Mise à jour des références des numéros de Vacarme',
-	
+
 	// U
 	'update_bouton_label' => "Mettre à jour les références de numéros",
 );
